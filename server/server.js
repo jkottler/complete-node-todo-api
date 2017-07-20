b0cc2ace587ccffc74d2b527390567cad3ed0fa4
Jason Kottler <jason.kottler@skyscanner.net>
2017-07-20 13:05:33 +0100

    Added first test to POST /todos

--- server/server.js
+++ server/server.js
@@ -11,16 +11,21 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  })
-})
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
 
 app.listen(3000, () => {
-  console.log('started on port 3000')
+  console.log('started on port 3000');
 });
+
+module.exports = { app };
